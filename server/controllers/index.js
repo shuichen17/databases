e7209419fc313bea081e080b31674b3e1eb23639
shuichen17 <shuichen17@gmail.com>
2019-05-02 12:32:21 -0700

    updating

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,11 @@
 var models = require('../models');
 
+//controller: 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
+    get: function (req, res) {
+      //
+    }, // a function which handles a get request for all messages
     post: function (req, res) {} // a function which handles posting a message to the database
   },
 
--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,27 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE User (
+  ID int NOT NULL AUTO_INCREMENT,
+  username varchar(50),
+  PRIMARY KEY (ID)
 );
+CREATE TABLE Room (
+  ID int NOT NULL AUTO_INCREMENT,
+  roomname varchar(50),
+  PRIMARY KEY (ID)
+);
+CREATE TABLE Messages (
+  /* Describe your table here.*/
+  messageID int NOT NULL AUTO_INCREMENT,
+  message varchar(255),
+  userID int,
+  roomID int,
+  PRIMARY KEY (messageID),
+  FOREIGN KEY (userID) REFERENCES User(ID),
+  FOREIGN KEY (roomID) REFERENCES Room(ID)
+);
+
 
 /* Create other tables and define schemas for them here! */
 
